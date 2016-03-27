31571335aedb13f6b89697734fe3105e00ba2caa
Raymond Zhu <raymond.zhu@ansarada.com>
2016-03-27 23:16:55 +1100

    web server

--- webserver/app.js
+++ webserver/app.js
@@ -5,6 +5,9 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 
+var subSocket = require('./lib/subscribe');
+var model = require('./models/badges');
+
 server.listen(3000, function(){
   console.log('listen at port %d', 3000);
 });
@@ -13,4 +16,17 @@ app.use(express.static('public'));
 
 app.get('/', function(req, res){
   res.sendfile('./public/index.html');
+});
+
+io.sockets.on('connection', function(socket){
+  model.get(function(err, data){
+    if (err) return;
+    data.forEach(function(badge){
+      socket.emit('badge', badge);
+    });
+  });
+});
+
+subSocket.on('message', function(message){
+  io.sockets.emit('badge', message);
 });
